5b7d0442faa68ff2b497786b84f8fdc6ef99cd52
Ethan Friedman <ethanjfriedman@gmail.com>
2015-06-04 09:40:43 -0400

    much more shell game

--- shell_game.js
+++ shell_game.js
@@ -5,14 +5,14 @@ var game = {
   numOfCards: 3,
   numOfSwaps: 5,
   guessedCard: false,
+  swapDuration: 1000,
+
   ball: {
     cardNumber: 1
   },
-
   randNum: function() {
     return Math.floor(Math.random() * 3);
   },
-
   initialize: function() {
     var Card = function(num) {
       this.hasBall = false;
@@ -25,23 +25,27 @@ var game = {
     this.cards[1].hasBall = true;
     views.initalizeViews();
   },
-
   play: function() {
     this.setNumOfSwaps();
+    this.setSwapTiming();
     views.ballView.hide();
     this.swap(this.randNum(),this.randNum());
   },
-
   setNumOfSwaps: function() {
     this.numOfSwaps = 4 + this.level;
   },
-
+  setSwapTiming: function() {
+    var $cards = $('.card');
+    this.swapDuration = this.swapDuration * (1 + (this.level - 1) / 10)
+    var duration = this.swapDuration + "ms";
+    // var duration = '0.4s';
+    $cards.css('transition-duration',duration);
+},
   swap: function(card1, card2) {
     if (this.swapCount < this.numOfSwaps) {
       while (card1 == card2) {
         card1 = this.randNum();
       }
-      //console.log("I'm swapping card no. " + card1 + " and card no. " + card2);
       this.cards[card1].position = card2;
       this.cards[card2].position = card1;
       this.sortCards(); //sorts the game.cards array by position. Otherwise multiple cards could have same position
@@ -51,14 +55,12 @@ var game = {
       this.swapCount++
       setTimeout(function(){
         this.swap(this.randNum(),this.randNum());
-      }.bind(this), 1010);
+      }.bind(this), this.swapDuration);
     }
-
     if (this.swapCount === this.numOfSwaps) {
       views.swapEnd();
     }
   },
-
   sortCards: function() {
     this.cards.sort(function(a,b) {
       return a.position - b.position;
@@ -70,22 +72,18 @@ var views = {
   cardViews: [],
   ballView: {
     ball: game.ball,
-
     hide: function() {
       $('#ball').addClass('hidden');
     },
-
     show: function() {
       $('#ball').removeClass('hidden');
       views.gameOver();
     },
-
     initialize: function() {
       var $ball = $('<p id="ball">');
       this.$el = $ball; // now ballView.$el is the one and only ball.
     },
   },
-
   CardView: function(num) { //creates a CardView constructor function
     this.card = game.cards[num];
     this.$el =  $('<div class="card">');
@@ -107,18 +105,15 @@ var views = {
     };
     this.setAttrs();
   },
-
   sortViews: function () {
     this.cardViews.sort(function(a,b) {
       return a.card.position - b.card.position;
     });
   },
-
   initalizeViews: function() {
     this.$gameboard = $('#gameboard');
     this.ballView.initialize();
-    //creates three new cardViews and add each to the dom
-    for (var i = 0; i < game.cards.length; i++) {
+    for (var i = 0; i < game.cards.length; i++) { //creates three new cardViews and add each to the dom
       var newCardView = new this.CardView(i);
       if (newCardView.card.hasBall) {
         newCardView.$el.append(this.ballView.$el);
@@ -128,14 +123,12 @@ var views = {
     };
     this.setStartButton();
   },
-
   swapEnd: function() {
     $display = $('#display');
     $display.text('So... which card is hiding the ball?');
     this.setCardEvents();
     $('#reset').removeClass('hidden');
   },
-
   gameOver: function() {
     $display = $('#display');
     if (game.guessedCard) {
@@ -145,21 +138,24 @@ var views = {
     }
     this.setResetButton();
   },
-
   setStartButton: function() {
     $start = $('#start'); //creates a jQuery object for the start button
     $display = $('#display') //..and one for the display text area
     $start.on('click', function(eventObject) {
+      $display.html('Follow the cards closely! Level: ' + game.level);
+      if (eventObject.shiftKey) {
+        game.level = 15;
+        $display.html('YOU HAVE ENTERED NYC MODE! BEWARE!');
+        game.swapDuration = 200;
+      }
       $start.addClass('hidden');
-      $display.html('Follow the cards closely!');
+
       game.play();
       if (game.guessedCard) {
         game.level++;
-        console.log(game.level);
       }
     });
   },
-
   setCardEvents: function() {
     var $cards = $('.card'); //jQuery object for the card elements in the DOM
     var $card0 = $($cards[0]);
@@ -179,16 +175,13 @@ var views = {
       views.ballView.show();
       });
   },
-
   setResetButton: function() {
     $reset = $('#reset');
     $reset.on('click',function(eventObject) {
-      //game.guessedCard = false;
       game.swapCount = 0;
       $reset.addClass('hidden');
       $('#start').removeClass('hidden');
     });
   }
 };
-
 game.initialize();
